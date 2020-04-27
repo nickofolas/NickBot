d12b398c07ed2d3c10a710a18c7e2267315d79a6
nickofolas <60761231+nickofolas@users.noreply.github.com>
2020-04-26 20:21:02 -0500

    Removed unnecessary release() from help

--- cogs/meta.py
+++ cogs/meta.py
@@ -166,7 +166,6 @@ class PaginatedHelpCommand(commands.HelpCommand):
         pages.get_page = pages.get_bot_page
         pages.is_bot = True
         pages.total = total
-        await self.context.release()
         await pages.paginate()
 
     async def send_cog_help(self, cog):
@@ -175,7 +174,6 @@ class PaginatedHelpCommand(commands.HelpCommand):
         pages.title = f'{cog.qualified_name} Commands'
         pages.description = cog.description
 
-        await self.context.release()
         await pages.paginate()
 
     def common_command_formatting(self, page_or_embed, command):
@@ -204,7 +202,6 @@ class PaginatedHelpCommand(commands.HelpCommand):
         if retrieve_checks(group):
             pages.embed.set_footer(text=f'Checks: {retrieve_checks(group)}')
 
-        await self.context.release()
         await pages.paginate()
 
 
