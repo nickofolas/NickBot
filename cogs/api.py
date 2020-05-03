8a545427526e7ed5244b695518cfff253f62f301
nickofolas <60761231+nickofolas@users.noreply.github.com>
2020-05-03 14:01:44 -0500

    - Added checks for fortnite stats

--- cogs/api.py
+++ cogs/api.py
@@ -383,7 +383,7 @@ class Api(commands.Cog):
         checked_recents = ['matches', 'kills', 'top1', 'top5', 'playersOutlived', 'minutesPlayed']
         e2 = max(checked_recents, key=lambda x: len(x))
         if rstats := js.get('recentMatches'):
-            while len(recents) < 1000:
+            while len(recents) < 500:
                 for i in rstats:
                     for c in checked_recents:
                         recents += f"{c.title().ljust(len(e2))} {i.get(c)}\n"
