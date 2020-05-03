30035eb0c316f7aed4da47698bedc19900a5608a
nickofolas <60761231+nickofolas@users.noreply.github.com>
2020-05-03 11:02:13 -0500

    - Improved format of fortnite item shop command

--- cogs/api.py
+++ cogs/api.py
@@ -352,7 +352,7 @@ class Api(commands.Cog):
             for cat, grp in itertools.groupby([*js], lambda c: c.get('storeCategory')):
                 yield f'**__{cat}__**\n<:vbuck:706533872460103731> **Price** {ctx.tab(12)} **Item**', '\n'.join(
                     sorted(
-                        [f"{g.get('vBucks')} [`{g.get('name')}`]({g.get('imageUrl')})"
+                        [f"`{g.get('vBucks'):<4}` [`{g.get('name')}`]({g.get('imageUrl')})"
                          for g in [*grp]]))
         await ctx.quick_menu(
             [*_gather()],
