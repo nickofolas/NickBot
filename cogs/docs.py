321ae31eeb526e2fc5caa743a42b31ee20b1a246
nickofolas <60761231+nickofolas@users.noreply.github.com>
2020-05-15 16:36:36 -0500

    - Fixed checks to ensure that inputs for todo and highlight removals are integers

--- cogs/docs.py
+++ cogs/docs.py
@@ -2,19 +2,6 @@
 # Copyright ©︎ 2015 Rapptz
 # R. Danny licensing:
 # https://github.com/Rapptz/RoboDanny
-"""
-The MIT License (MIT)
-Copyright (c) 2015 Rapptz
-Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the "Software"),
-to deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-"""
 import re
 import io
 import os
